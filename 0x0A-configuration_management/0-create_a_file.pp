32ab92421f6e49b3fd5b629133a9a68221ae3efa
Javan <javan.o.otieno@gmail.com>
2023-06-24 02:11:38 +0300

    Fix: fixed linting problem

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { '/tmp/school':
   content => 'I love Puppet',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
