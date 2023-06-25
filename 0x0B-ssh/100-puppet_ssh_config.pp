05312b029447ab07e70f33bff273f57f7e73beb7
Javan Odhiambo <114527546+Javan-Odhiambo@users.noreply.github.com>
2023-06-25 18:31:40 +0300

    Initial solution

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+include stdlib
+
+file_line { 'Disable password authentication':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Specify the identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
